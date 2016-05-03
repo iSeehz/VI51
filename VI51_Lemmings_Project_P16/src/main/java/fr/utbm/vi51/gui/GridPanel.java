65892d01d43fc00b85086b7d6b19cbebb8d08629
Tiboty <thibauldjardino>
2016-05-03 17:22:50 +0200

    utilisation de JPanel pour la grid GUI

--- VI51_Lemmings_Project_P16/src/main/java/fr/utbm/vi51/gui/GridPanel.java
+++ VI51_Lemmings_Project_P16/src/main/java/fr/utbm/vi51/gui/GridPanel.java
@@ -1,9 +1,10 @@
 package fr.utbm.vi51.gui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.List;
 
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 import fr.utbm.vi51.model.Cell;
@@ -15,19 +16,21 @@ public class GridPanel extends JPanel{
 	private static final long serialVersionUID = -4072497686393047571L;
 	private int width;
 	private int height;
-	private Object grid [][];
+	private JPanel grid [][];
 	public GridPanel(List<List<Cell>> tab) {
 		// Initialize the grid
 		this.width = tab.size();
 		this.height = tab.get(0).size();
-		this.grid = new Object[this.height][this.width];
+		this.grid = new JPanel[this.height][this.width];
 		
 		this.setLayout(new GridLayout(width,height));
 		for (int i = 0;i<this.height;i++){
 			
 			for (int j = 0;j<this.width;j++){
-				//grid[i][j]
-				this.add(new JButton());
+				grid[i][j] = new JPanel();
+				grid[i][j].setBackground(Color.WHITE);
+				grid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				this.add(grid[i][j]);
 			}
 		}
 	}
